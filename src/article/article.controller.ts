41d8cbe8d5c6f88c758a56f6703684be45e5d32d
ivanchenko9 <denigoriv@gmail.com>
2022-06-09 13:24:14 +0300

    feat: added findBySlug()

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,7 +1,7 @@
 import { User } from '@app/user/decorators/user.decorator';
 import { AuthCuard } from '@app/user/guards/auth.guard';
 import { UserEntity } from '@app/user/user.entity';
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { CreateArticleDto } from './dto/createArticle.dto';
 import { ArticleResponceInterface } from './types/articleResponce.interface';
@@ -19,6 +19,14 @@ export class ArticleController {
       currentUser,
       createArticleDto,
     );
+    return this.articleService.buildArticleResponse(article);
+  }
+
+  @Get(':slug')
+  async getArticleBySlug(
+    @Param('slug') slug: string,
+  ): Promise<ArticleResponceInterface> {
+    const article = await this.articleService.findBySlug(slug);
     return this.articleService.buildArticleResponse(article);
   }
 }
